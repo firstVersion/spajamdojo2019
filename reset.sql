089ce5653a11102422cb65fb6f1ea1c5085b3dd8
Sota Yano <kuroisiratama@yahoo.co.jp>
2019-04-21 00:28:46 +0900

    Added reset.sql

---
+++ reset.sql
@@ -0,0 +1,11 @@
+\c spajamdojo2019
+DROP TABLE scores;
+DROP TABLE bets;
+DROP TABLE terms;
+DROP TABLE users;
+\i sql/db_spajamdojo2019.sql
+\i sql/table01_users.sql
+\i sql/table02_scores.sql
+\i sql/table03_bets.sql
+\i sql/table04_terms.sql
+\i sql/table05_data.sql
